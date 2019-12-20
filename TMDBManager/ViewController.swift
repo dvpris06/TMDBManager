5fc3377e7c0cd58ddb9d5750f0e29273f935e6b6
Dmitry <dvpris06@gmail.com>
2019-12-20 14:46:33 +0300

    Initial Commit

---
+++ TMDBManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDBManager
+//
+//  Created by dragdimon on 20/12/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
