08eb4e88cb1e62581cf7244ac3bd4575ba31ef33
Dmitry <dvpris06@gmail.com>
2019-12-22 16:10:14 +0300

    Add LoginVC view

--- TMDBManager/Controller/LoginViewController.swift
+++ TMDBManager/Controller/LoginViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginViaWebsite: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
